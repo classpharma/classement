2fa1435e6ae03da255de65d1b2e584233cd34868
classpharma <mjcf.lover.yaoi@gmail.com>
2025-03-16 11:56:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -84,4 +84,5 @@ def index():
     return render_template("index.html", result=result, error=error)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Récupère le port depuis l'environnement, sinon utilise 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
